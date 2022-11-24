de68fa9932498948c98e2808749d93d8f61f312e
Gene Sipes <gsipes00@yahoo.com>
2022-11-24 13:07:54 -0600

    set up app js to fetch data and change state with useeffect

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,45 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-projects";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  // set the state for the component
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  // fetch data from api
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  // useEffect to update component state
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  // show loading screen
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  // return data
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
-export default App
+export default App;
