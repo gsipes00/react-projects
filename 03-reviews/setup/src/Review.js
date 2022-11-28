b9adcedc4d6b8a51ab68bcabf25c499ed752aacf
gsipes00 <gsipes00@yahoo.com>
2022-11-28 17:11:27 -0600

    stuff

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import { FaArrowAltCircleLeft, FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0)
+  const {name, jon, image, text} = people[index]
+  return (
+    article
+  )
 };
 
 export default Review;
