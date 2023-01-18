3f1f4eade2af7dd8fc416c7c611c2c5a330e5282
Gene Sipes <gsipes00@yahoo.com>
2023-01-17 21:11:59 -0600

    built out submit, list, and alerts

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,7 +3,65 @@ import List from "./List";
 import Alert from "./Alert";
 
 function App() {
-  return <h2>grocery bud setup</h2>;
+  // state values
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: "",
+    type: "",
+  });
+
+  // event handler func for submit button
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name);
+    if (!name) {
+      // display alert
+      showAlert(true, "danger", "please enter a value");
+    } else if (name && isEditing) {
+      // deal with edit
+    } else {
+      //  show alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
+  };
+  // show alerts function
+  const showAlert = (show = false, type = "", msg = "") => {
+    // ES6 shorthand, passing in params
+    setAlert({ show, type, msg });
+  };
+  // return jsx
+  return (
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
+        <h3>grocery bud</h3>
+        <div className='form-control'>
+          <input
+            type='text'
+            className='grocery'
+            placeholder='e.g. eggs...'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type='submit' className='submit-btn'>
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className='grocery-container'>
+          <List items={list} />
+          <button className='clear-btn'>clear items</button>
+        </div>
+      )}
+    </section>
+  );
 }
-// this is a test comment
+
 export default App;
