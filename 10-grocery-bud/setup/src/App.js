0e23aa040ed7db9fb8c92337bc249e2b2bb7ce1f
Gene Sipes <gsipes00@yahoo.com>
2023-01-17 21:57:33 -0600

    added remove functionality to delte button, and added List variable to the useEffect rerender array

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -43,11 +43,18 @@ function App() {
     showAlert(true, "danger", "list emptied");
     setList([]);
   };
+
+  // remove item
+  const removeItem = (id) => {
+    showAlert(true, "danger", "item removed from list");
+    setList(list.filter((item) => item.id !== id));
+  };
+
   // return jsx
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
+        {alert.show && <Alert {...alert} showAlert={showAlert} list={list} />}
         <h3>grocery bud</h3>
         <div className='form-control'>
           <input
@@ -64,7 +71,7 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className='grocery-container'>
-          <List items={list} />
+          <List items={list} removeItem={removeItem} />
           <button className='clear-btn' onClick={clearList}>
             clear items
           </button>
